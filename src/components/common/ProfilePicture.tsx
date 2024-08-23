a654cb88674751491bd549096415412d1eb8977c
Ddaniel Ccarlos <daniel.hammerdev@gmail.com>
2024-08-23 18:14:35 -0300

    profile page

--- src/components/common/ProfilePicture.tsx
+++ src/components/common/ProfilePicture.tsx
@@ -1,13 +1,15 @@
+import { ImgHTMLAttributes } from "react";
 import { getAvatarURL } from "../../api/staticFilesAPI";
 import { User } from "../../types/mainTypes";
 
 interface ProfilePictureProps {
-    user?: User
+    user?: User,
+    rest?: ImgHTMLAttributes<any>
 }
 
-export const ProfilePicture = ({ user }: ProfilePictureProps) => {
+export const ProfilePicture = ({ user, ...rest }: ProfilePictureProps) => {
 
     return (
-        <img className="avatar" src={getAvatarURL(user!.id)} width={"100%"} height={"100%"} style={{ borderRadius: "50%" }} />
+        <img className="avatar" src={getAvatarURL(user!.id)} width={"100%"} height={"100%"} style={{ borderRadius: "50%" }} {...rest} />
     )
 }
--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,6 +1,17 @@
+import { ProfilePicture } from "../../components/common/ProfilePicture"
+import { useAuth } from "../../contexts/auth/authContext"
 import "./ProfilePage.css"
 
 export const ProfilePage = () => {
+    const { me } = useAuth();
 
-    return <><h1>Profile Page</h1></>
+    return <div id="profile-page" className="full">
+        <div id="profile-card" className="shadow">
+            <div id="profile-picture-container">
+                <ProfilePicture user={me!} />
+            </div>
+            <h1>{me?.name}</h1>
+            <em>{me?.username}</em>
+        </div>
+    </div>
 }
